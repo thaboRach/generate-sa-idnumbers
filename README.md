40396623f8820ab2bdb7be588519eba6d9542497
thaboRach <masipathabo.rachidi@vcontractor.co.za>
2023-01-23 20:14:31 +0200

    create site

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# generate-sa-idnumbers
-Generate (fake) South African ID Numbers
+# Generate South African ID Numbers
 
-Use it [here](https://chris927.github.io/generate-sa-idnumbers/).
+## About
+
+Generate a 13-digit South African ID.
+
+**Please note that some of the ID numbers you generate may belong to a real person. This was only created for educational and software testing use only. I will not be liable for any misuse**
+
+## Credits
+
+- [Chris927](https://github.com/Chris927/generate-sa-idnumbers)
+- [dj-louw](https://github.com/dj-louw/generate-sa-idnumbers)
+
+## Licence
+
+LGPL-3.0 license
---
+++ script.js
@@ -0,0 +1,103 @@
+const form = document.forms.mainForm;
+const resultArea = document.getElementById("result");
+
+// calculates the Lohn digit, see https://en.wikipedia.org/wiki/Luhn_algorithm
+function calculateCheckDigit(digitsAsString) {
+  //const digits = ('' + dob + gender + sequence + citizenship + eight).split('').map(d => Number(d))
+  const digits = digitsAsString
+    .replace(/\D/g, "")
+    .split("")
+    .map((d) => Number(d));
+  const checkSum = digits
+    .reverse()
+    .map((d, ix) => {
+      if (ix % 2 === 0) {
+        d *= 2;
+        if (d > 9) {
+          d -= 9;
+        }
+      }
+      return d;
+    })
+    .reduce((memo, d) => (memo += d), 0);
+  return (checkSum * 9) % 10;
+}
+
+// show a valid ID number generated using the values in the form
+function showIdNumber() {
+  const values = [
+    form.year,
+    form.month,
+    form.day,
+    form.gender,
+    { value: form.sequence.value || "896" /* sequence */ },
+    { value: form.cship.value || "0" /* citizenship */ },
+    { value: "8" /* a */ },
+  ];
+
+  withoutCheckDigit = values.map((e) => e.value).join("");
+  const idNumber = withoutCheckDigit + calculateCheckDigit(withoutCheckDigit);
+
+  resultArea.innerHTML = `${idNumber}`;
+}
+
+// Generates a random integer between a min and a max
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
+function generateRandomIdNumber() {
+  console.log("click");
+  const values = [
+    { value: getRndInteger(50, 99) /* year */ },
+    { value: ("00" + getRndInteger(1, 12)).slice(-2) /* month */ },
+    { value: ("00" + getRndInteger(1, 31)).slice(-2) /* day */ },
+    { value: getRndInteger(4, 5) /* gender */ },
+    { value: ("000" + getRndInteger(0, 999)).slice(-3) /* sequence */ },
+    { value: getRndInteger(0, 1) /* citizenship */ },
+    { value: "8" /* a */ },
+  ];
+
+  withoutCheckDigit = values.map((e) => e.value).join("");
+  const idNumber = withoutCheckDigit + calculateCheckDigit(withoutCheckDigit);
+
+  resultArea.innerHTML = `${idNumber}`;
+}
+
+// add 'option' elements to the 'select' elements (for year, month, day)
+function addOptions(id, from, to, toLabel, toValue, defaultValue) {
+  const selectElement = document.getElementById(id);
+  const values = [];
+  for (let i = 0; i <= to - from; i++) values[i] = i + from;
+  const options = values
+    .map((v) => (v < 10 ? "0" : "") + v)
+    .map((v) => {
+      const value = toValue ? toValue(v) : v;
+      const isDefault = defaultValue && defaultValue === value;
+      const label = toLabel ? toLabel(v) : v;
+      return `<option value=${value} ${
+        isDefault ? 'selected="selected"' : ""
+      }>${label}</option>`;
+    });
+
+  selectElement.innerHTML = options.join();
+}
+
+const year = new Date().getUTCFullYear();
+const optionYear = year - 18;
+
+const pad = function (n) {
+  const str = "" + n;
+  const pad = "000";
+  return pad.substring(0, pad.length - str.length) + str;
+};
+
+addOptions(
+  "year",
+  1950,
+  optionYear,
+  (v) => `${v} (${year - Number(v)} years old)`
+);
+addOptions("month", 1, 12);
+addOptions("day", 1, 31);
+addOptions("sequence", 0, 999, (v) => pad(v), pad, "800");
